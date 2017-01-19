1cea959fc0e02b9839839a073b6ace4a2ea198ce
Vincenzo De Vivo <deltavi79@gmail.com>
2017-01-19 23:44:17 +0100

    Added MimeTypesUtils

---
+++ src/main/java/com/vincenzodevivo/jdutils/MimeTypesUtils.java
@@ -0,0 +1,19 @@
+package com.vincenzodevivo.jdutils;
+
+import javax.activation.MimetypesFileTypeMap;
+
+/**
+ * Created by Vincenzo De Vivo on 19/01/2017.
+ */
+public class MimeTypesUtils {
+    /**
+     * Get content type from file extension
+     * @param fileName
+     * @return content type
+     */
+    public static String getFileContentType(final String fileName) {
+        return MimetypesFileTypeMap
+                .getDefaultFileTypeMap()
+                .getContentType(fileName);
+    }
+}
---
+++ src/test/java/com/vincenzodevivo/jdutils/MimeTypesUtilsTest.java
@@ -0,0 +1,18 @@
+package com.vincenzodevivo.jdutils;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Vincenzo De Vivo on 19/01/2017.
+ */
+public class MimeTypesUtilsTest extends TestCase {
+    public void testGetFileContentType_pdf() throws Exception {
+        String contentType = MimeTypesUtils.getFileContentType("C:\\test\\testFile.pdf");
+        assertEquals("Content type mismatch", "application/pdf", contentType);
+    }
+
+    public void testGetFileContentType_txt() throws Exception {
+        String contentType = MimeTypesUtils.getFileContentType("testFile.txt");
+        assertEquals("Content type mismatch", "text/plain", contentType);
+    }
+}
